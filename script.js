da8f5d43e81e297c495587e06286c9a491fe7ed1
David Chou <davidchou2009@gmail.com>
2021-12-30 23:17:42 -0800

    add timer

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var quizEl = document.querySelector("#quiz");
 var resultsEl = document.querySelector("#results");
 var startEl = document.querySelector("#start");
-var choiceEl = document.querySelector("#choiceSelection")
+var choiceEl = document.querySelector("#choice")
 var quiz = [];
 
 var questions = [
@@ -71,78 +71,68 @@ var questions = [
 
 //create functions
 
-// function createQuiz(){
+function createQuiz(){
+        //create timer and update time every second
+        var timerInterval = setInterval(function() {
+        
+            timer--;
+            timeEl.textContent = "Time Left: " + timer;
+    
+            if(timer === 0 || timer <=0){
+                timer = 0; //if timer is at 5 seconds, and user gets question wrong, timer 
+                //can become negative.
+                clearInterval(timerInterval);
+                endGame(); //end game when timer reaches 0
+                //Go display results
+            }
+        }, 1000);
 
-//     function displayQuestions(){
+    function displayQuestions(){
 
-//         //create buttons and append
-
-//         for(let i = 0 ; i < questions.length ; i++){
-//             var choiceA = document.createElement("button");
-//             var choiceB = document.createElement("button");
-//             var choiceC = document.createElement("button");
-//             var choiceD = document.createElement("button");
-
-//             choicesEl.appendChild(choiceA);
-//             choicesEl.appendChild(choiceB);
-//             choicesEl.appendChild(choiceC);
-//             choicesEl.appendChild(choiceD);
-
-//             quizEl.children[0].textContent = questions[i].question;
-//             choiceA.textContent = "1." + questions[i].answers.a;
-//             choiceB.textContent = "2." +  questions[i].answers.b;
-//             choiceC.textContent = "3." +  questions[i].answers.c;
-//             choiceD.textContent = "4." +  questions[i].answers.d;
-//         }
-
-
-
-//     }
-
-//     function getResults(){
-
-//     }
-
-//     displayQuestions();
-//     getResults();
-
-// }
-function displayQuestions(){
-
-    startEl.setAttribute("style","display:none");
-    quizEl.children[0].setAttribute("style","display:none");
-    quizEl.children[1].setAttribute("style","display:none");
-    //create buttons and append
-
-    for(let i = 0 ; i < questions.length ; i++){
-
-        var currentQ = questions[i].question;
-        var question = document.createElement("div");
-        question.setAttribute("class","question");
-        choiceEl.appendChild(question);
-        question.textContent = currentQ;
-
-        var choiceA = document.createElement("button");
-        var choiceB = document.createElement("button");
-        var choiceC = document.createElement("button");
-        var choiceD = document.createElement("button");
-
-        choiceEl.appendChild(choiceA);
-        choiceEl.appendChild(choiceB);
-        choiceEl.appendChild(choiceC);
-        choiceEl.appendChild(choiceD);
-
-
-        choiceA.textContent = "1." + questions[i].answers.a;
-        choiceB.textContent = "2." +  questions[i].answers.b;
-        choiceC.textContent = "3." +  questions[i].answers.c;
-        choiceD.textContent = "4." +  questions[i].answers.d;
+        startEl.setAttribute("style","display:none");
+        quizEl.children[0].setAttribute("style","display:none");
+        quizEl.children[1].setAttribute("style","display:none");
+        //create buttons and append
+    
+        for(let i = 0 ; i < questions.length ; i++){
+    
+            var currentQ = questions[i].question;
+            var question = document.createElement("div");
+            question.setAttribute("class","question");
+            choiceEl.appendChild(question);
+            question.textContent = currentQ;
+    
+            var choiceA = document.createElement("button");
+            var choiceB = document.createElement("button");
+            var choiceC = document.createElement("button");
+            var choiceD = document.createElement("button");
+    
+            choiceEl.appendChild(choiceA);
+            choiceEl.appendChild(choiceB);
+            choiceEl.appendChild(choiceC);
+            choiceEl.appendChild(choiceD);
+    
+    
+            choiceA.textContent = "1." + questions[i].answers.a;
+            choiceB.textContent = `2.${questions[i].answers.b}`;
+            choiceC.textContent = "3." +  questions[i].answers.c;
+            choiceD.textContent = "4." +  questions[i].answers.d;
+        }
+    
+    
+    
     }
 
+    function getResults(){
 
+    }
+
+    displayQuestions();
+    getResults();
 
 }
-startEl.addEventListener("click",displayQuestions);
+
+startEl.addEventListener("click",createQuiz);
 // function endQuiz();
 
 
